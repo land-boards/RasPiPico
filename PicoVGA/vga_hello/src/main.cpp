6d4be7f546c4089de7a44f2a1aab9e8738c12a93
Doug Gilliland <doug@douglasgilliland.com>
2022-07-17 03:46:22 -0400

    PicoVGA design

--- PicoVGA/vga_hello/src/main.cpp
+++ PicoVGA/vga_hello/src/main.cpp
@@ -16,6 +16,6 @@ int main()
 	Video(DEV_VGA, RES_EGA, FORM_8BIT, Box);
 
 	// draw text
-	DrawText(&Canvas, "Hello World!", (512-12*8*4)/2, (400-8*4)/2,
+	DrawText(&Canvas, "Hello Doug!", (512-12*8*4)/2, (400-8*4)/2,
 		COL_WHITE, FontBoldB8x16, 16, 4, 4);
 }
