1e165491fef101fa8d88fa4b7d29e76f12aa0bd3
Doug Gilliland <doug@douglasgilliland.com>
2022-08-21 03:32:14 -0400

    Build Template - empty Arduino style fcns

---
+++ PicoVGA/Build_Template/src/main.cpp
@@ -0,0 +1,30 @@
+// Build Template
+// Coded in Arduino style with setup() and loop() style
+// Runs on 
+//	http://land-boards.com/blwiki/index.php?title=PicoVGA01
+
+// #include <stdio.h>
+// #include "pico/stdlib.h"   // stdlib 
+// #include "hardware/irq.h"  // interrupts
+// #include "hardware/pwm.h"  // pwm 
+// #include "hardware/sync.h" // wait for interrupt 
+ 
+#define AUDIO_PIN 19
+
+int setup(void)
+{
+	return(0);
+}
+
+int loop(void)
+{
+	return(0);
+}
+
+int main(void) {
+	setup();
+    while(1);
+	{
+		loop();
+    }
+}
